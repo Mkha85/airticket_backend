1ac87a531cdccfbbb3fa31044e5cc5be5e85a7c5
Duc Dang <ducstray@gmail.com>
2022-12-25 20:32:15 +0700

    init

---
+++ AirTicket-master/README.md
@@ -0,0 +1,20 @@
+[![Buy Me A Coffee](https://img.shields.io/badge/Donate-Buy%20Me%20A%20Coffee-yellow.svg)](https://www.buymeacoffee.com/rohanprasad)
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://paypal.me/logarithm4?locale.x=en_GB)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## AirTicket
+  REST APIs for Airticket Android App hosted at: https://github.com/rohanprasadofficial/Airticket-App 
+### How to run
+- Clone the repository
+- Start mongodb server
+- Add a env file
+- Add into it 
+  `EXPORT username="XXX"
+   EXPORT password="XXX"
+   EXPORT DBURL="XXX"
+  `
+- Run `source env`
+- Add env file to .gitignore
+- Run `npm i`
+- Run `npm start`
---
+++ AirTicket-master/server.js
@@ -0,0 +1,66 @@
+var express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const passport = require("passport");
+
+/**
+ *ROLE ID : 
+  Admin - 2
+  User - 1 
+ */
+
+const app = express();
+const port = 3500 || process.env.PORT;
+
+//Passport Middleware
+app.use(passport.initialize());
+require("./strategy/jsonwtStrategy")(passport);
+
+// app.use(express.static(path.join(__dirname, "public")));
+// app.set("view engine", "ejs");
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Importing all the routes/api
+const auth = require("./routes/api/auth");
+const booking = require("./routes/api/booking");
+const flight = require("./routes/api/flight");
+
+app.use("/auth", auth);
+app.use("/booking", booking);
+app.use("/flight", flight);
+
+var db = require("./setup/DBSetup").DBURL;
+var usernameDB = require("./setup/DBSetup").username;
+var passwordDB = require("./setup/DBSetup").password;
+
+const authData = {
+  // user: usernameDB,
+  // pass: passwordDB,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(
+  "mongodb+srv://kha123:kha123@cluster0.6phm7lc.mongodb.net/ticket?retryWrites=true&w=majority",
+  () => {
+    console.log("Mongoose Connected");
+  }
+);
+
+//Basic Checking of Server
+app.get("/", (req, res) => {
+  res.send("<p>Airticket Server :)</p>");
+});
+
+app.get("/test", (req, res) => {
+  res.send("<p>This is test  from Rohan</p>");
+});
+
+//Starting Server
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
